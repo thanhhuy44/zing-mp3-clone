c8e325e89db82a9f2da476ab86331777da63a4ef
thanhhuy44 <90882506+thanhhuy44@users.noreply.github.com>
2022-08-10 15:52:09 +0700

    update

--- src/pages/Radio/RadioChannel.js
+++ src/pages/Radio/RadioChannel.js
@@ -1,7 +1,7 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+import { faPauseCircle, faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import images from '~/assets';
 import styles from './Radio.module.scss';
@@ -9,6 +9,8 @@ import {
     setInfoSongPlayer,
     setIsPlay,
     setIsRadioPlay,
+    setPlaylistId,
+    setPlaylistRandom,
     setPlaylistSong,
     setSrcRadio,
 } from '~/redux/features/audioSlice';
@@ -17,12 +19,16 @@ const cx = classNames.bind(styles);
 
 function RadioChannel({ data }) {
     const dispatch = useDispatch();
+    const songInfo = useSelector((state) => state.audio.infoSongPlayer);
+    const isRadioPlay = useSelector((state) => state.audio.isRadioPlay);
     const handlePlayRadio = (data) => {
         dispatch(setSrcRadio(data.streaming));
         dispatch(setInfoSongPlayer(data));
         dispatch(setIsRadioPlay(true));
         dispatch(setIsPlay(false));
         dispatch(setPlaylistSong([]));
+        dispatch(setPlaylistRandom([]));
+        dispatch(setPlaylistId(''));
     };
 
     return (
@@ -30,13 +36,25 @@ function RadioChannel({ data }) {
             <img src={data.thumbnailH} className={cx('channel-bg')} alt={data.program.title} />
             <div className={cx('content')}>
                 <div className={cx('avatar')}>
-                    <div className={cx('thumb')}>
+                    <div className={cx('thumb', isRadioPlay && songInfo.encodeId === data.encodeId && 'playing')}>
                         <img src={data.thumbnailM} alt={data.description} />
                         <img src={images.liveLabel} className={cx('label')} alt="label" />
                     </div>
-                    <div className={cx('action')} onClick={() => handlePlayRadio(data)}>
-                        <FontAwesomeIcon icon={faPlayCircle} />
-                    </div>
+                    {songInfo.encodeId !== data.encodeId && (
+                        <div className={cx('action')} onClick={() => handlePlayRadio(data)}>
+                            <FontAwesomeIcon icon={faPlayCircle} />
+                        </div>
+                    )}
+                    {songInfo.encodeId === data.encodeId && isRadioPlay && (
+                        <div className={cx('action')} onClick={() => dispatch(setIsRadioPlay(!isRadioPlay))}>
+                            <FontAwesomeIcon icon={faPauseCircle} />
+                        </div>
+                    )}
+                    {songInfo.encodeId === data.encodeId && !isRadioPlay && (
+                        <div className={cx('action')} onClick={() => dispatch(setIsRadioPlay(!isRadioPlay))}>
+                            <FontAwesomeIcon icon={faPlayCircle} />
+                        </div>
+                    )}
                 </div>
                 <div className={cx('info')}>
                     <h2 className={cx('name')}>{data.title}</h2>
